76670a66960e6fc01ae30fabed46ae4ca9cf9925
grace <gracexinyi.wang@mail.utoronto.ca>
2022-10-30 08:33:12 +0800

    dataset and qsranking test

--- new_cms/src/main/java/com/example/cms/model/entity/Qs_rankings.java
+++ new_cms/src/main/java/com/example/cms/model/entity/Qs_rankings.java
@@ -14,12 +14,45 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name="QSRanking")
 
 public class Qs_rankings {
 
     @Id
     @NotEmpty
+    private String QS_ranking_id;
 
-    private String qs_ranking;
+    @NotEmpty
+    private String Institution_Name;
+
+    @NotEmpty
+    private String Location;
+
+    @NotEmpty
+    private String Academic_Reputation_Score;
+
+    @NotEmpty
+    private String Employer_Reputation_Score;
+    @NotEmpty
+    private String Faculty_Student_Score;
+
+    @NotEmpty
+    private String Citations_per_Faculty_Score;
+
+    @NotEmpty
+    private String International_Faculty_Score;
+
+    @NotEmpty
+    private String International_Students_Score;
+
+    @NotEmpty
+    private String International_Research_Network_Score;
+
+    @NotEmpty
+    private String Employment_Outcomes_Score;
+
+    @NotEmpty
+    private String Overall_Score;
+
 
 }
