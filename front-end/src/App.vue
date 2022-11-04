6d851956270a2b196fe747175459053e683c9498
CodefromMax <mr.wang@mail.utoronto.ca>
2022-11-04 19:20:22 -0400

    Update the interst list.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,18 +3,17 @@
   <b-container>
     <div>
       <b-navbar toggleable type="dark" variant="dark">
-        <router-link :to="{ path: '/interest_list' }">
+        <router-link :to="{ path: '/uni' }">
       <b-button size="lg" style="height: 50px;">Interest List</b-button></router-link>
         <b-navbar-brand href="#"><b-icon icon="book" variant="warning" style="margin-left: 20px;"></b-icon> U Future </b-navbar-brand>
 
       <b-navbar-toggle target="sidebar-menu">
       </b-navbar-toggle>
-      <b-sidebar id="sidebar-menu" title="MIE350 University" backdrop-variant="dark" backdrop shadow>
+      <b-sidebar id="sidebar-menu" title="U future" backdrop-variant="dark" backdrop shadow>
         <b-nav vertical align="center">
           <b-nav-item to="/" exact exact-active-class="exact-active">Home</b-nav-item>
           <b-nav-item to="/qsrankings" exact exact-active-class="exact-active">Qs rankings</b-nav-item>
-          <b-nav-item to="/professors" exact exact-active-class="exact-active">Professors</b-nav-item>
-          <b-nav-item to="/interest_list" exact exact-active-class="exact-active">Interest list</b-nav-item>
+          <b-nav-item to="/uni" exact exact-active-class="exact-active">Interest List</b-nav-item>
           <b-nav-item disabled>Admin</b-nav-item>
         </b-nav>
       </b-sidebar>
